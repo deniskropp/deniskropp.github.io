b8505142f6dbe67d391075df0ff31f0a18690ea8
Denis Kropp <Leichenbegatter@outlook.com>
2022-08-10 16:21:27 +0000

    ah

--- pages/Search.tsx
+++ pages/Search.tsx
@@ -6,11 +6,22 @@ import { Heads } from '../components/Heads'
 import { Mains } from '../components/Mains'
 
 const Search: NextPage = () => {
+  function handleSubmit(e: any)
+  {
+    console.log(e)
+  }
+
   return (
     <div className={styles.container}>
       <Heads />
 
       <Mains title="Search" desc="Find resources on the net...">
+        <div className={styles.card}>
+          <form>
+            <input type="text" name="q" placeholder="Search for resources"></input>
+            <input type="button" onClick={handleSubmit} value="Submit"></input>
+          </form>
+        </div>
       </Mains>
 
       <Footer />
